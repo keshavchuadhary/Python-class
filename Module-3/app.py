a19977f91dba05275f00c88e7ad1879af0f425dc
Keshav <keshavchy9841@gmail.com>
2024-02-18 15:02:26 +0545

    pip install

---
+++ Module-3/app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+
+st.write("hello guys k xajjjjj-")
+st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/TEIDE.JPG/1920px-TEIDE.JPG', caption='Sunrise by the mountains')
+from flask import Flask
